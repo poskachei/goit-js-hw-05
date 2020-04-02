22c2b8f03250cfc5d5f37037511d40bf3d6009b2
poskachei <vposkachey@gmail.com>
2020-04-02 22:31:05 +0300

    original

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class User {
+  constructor(name, age, followers) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`,
+    );
+  }
+}
+
+const mango = new User('Mango', 2, 20);
+
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const poly = new User('Poly', 3, 17);
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
